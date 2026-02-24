eea4aeabe420b76049990768f88ead08177ce51b
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-02-24 17:17:58 -0600

    termine el codigo

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
-public static void main (String)
+public static void main (String[] args){
+    Vehiculo vehiculo = new Vehiculo("Totoya" , "Corolla", 180.0);
+    vehiculo.mostrarInformacion();
+
+    Motocicleta moto = new Motocicleta("Honda","CBR600",250,600);
+    moto.mostrarinformacion();
+
+
+
+}
