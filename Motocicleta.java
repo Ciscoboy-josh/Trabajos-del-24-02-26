ec7ae23e9311b972a635d718fc2c0d4568565591
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-02-24 17:01:32 -0600

    agrege la motocicleta y el overdrive

---
+++ Motocicleta.java
@@ -0,0 +1,17 @@
+public class Motocicleta {
+    double cilindrada;
+
+    public Motocicleta(String marca, String modelo, double velocidadMaxima , double cilindrada){
+        this.cilindrada = cilindrada;
+
+
+    }
+     @Override
+    public void mostrarinformacion() {
+
+
+
+    }
+
+    
+}
